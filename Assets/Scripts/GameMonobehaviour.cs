6ea762f7c5da6c1b5e4d74b2a06c0b79e0450e5f
Neil Sarkar <neil.r.sarkar@gmail.com>
2019-04-23 22:20:53 -0700

    Allow movement of one player

---
+++ Assets/Scripts/GameMonobehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMonobehaviour : MonoBehaviour {
+	public int numPlayers;
+	public float gridSize = 0.6f;
+
+
+	GameObject playerOne;
+
+	void Start() {
+		playerOne = GameObject.Find("Player One");
+	}
+
+	void Update() {
+		var translation = Vector2.zero;
+
+		if (Input.GetKeyUp(KeyCode.RightArrow)) {
+			translation.x = gridSize;
+		} else if(Input.GetKeyUp(KeyCode.LeftArrow)) {
+			translation.x = -gridSize;
+		}
+
+		if (Input.GetKeyUp(KeyCode.UpArrow)) {
+			translation.y = gridSize;
+		} else if(Input.GetKeyUp(KeyCode.DownArrow)) {
+			translation.y = -gridSize;
+		}
+
+		playerOne.transform.Translate(translation);
+	}
+}
