2fbca4af45219da1289c729dcf3aadb7b211558c
Neil Sarkar <neil.r.sarkar@gmail.com>
2019-04-23 22:28:09 -0700

    Position players on grid at startup

--- Assets/Scripts/GameMonobehaviour.cs
+++ Assets/Scripts/GameMonobehaviour.cs
@@ -3,29 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameMonobehaviour : MonoBehaviour {
-	public int numPlayers;
-	public float gridSize = 0.6f;
-
+	public int gridSize = 5;
+	public float gridSquareWidth = 0.6f;
 
 	GameObject playerOne;
+	GameObject playerTwo;
 
 	void Start() {
 		playerOne = GameObject.Find("Player One");
+		playerTwo = GameObject.Find("Player Two");
+
+		playerOne.transform.position = new Vector2(gridSize * -gridSquareWidth / 2,0);
+		playerTwo.transform.position = new Vector2(gridSize * gridSquareWidth / 2,0);
 	}
 
 	void Update() {
 		var translation = Vector2.zero;
 
 		if (Input.GetKeyUp(KeyCode.RightArrow)) {
-			translation.x = gridSize;
-		} else if(Input.GetKeyUp(KeyCode.LeftArrow)) {
-			translation.x = -gridSize;
+			translation.x = gridSquareWidth;
+		} else if (Input.GetKeyUp(KeyCode.LeftArrow)) {
+			translation.x = -gridSquareWidth;
 		}
 
 		if (Input.GetKeyUp(KeyCode.UpArrow)) {
-			translation.y = gridSize;
-		} else if(Input.GetKeyUp(KeyCode.DownArrow)) {
-			translation.y = -gridSize;
+			translation.y = gridSquareWidth;
+		} else if (Input.GetKeyUp(KeyCode.DownArrow)) {
+			translation.y = -gridSquareWidth;
 		}
 
 		playerOne.transform.Translate(translation);
