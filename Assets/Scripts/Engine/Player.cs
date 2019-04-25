0b988f1c4bcd93dcb7384f1bb45268165dce9f47
Neil Sarkar <neil.r.sarkar@gmail.com>
2019-04-24 20:12:35 -0700

    Display cards from player hand

--- Assets/Scripts/Engine/Player.cs
+++ Assets/Scripts/Engine/Player.cs
@@ -9,13 +9,36 @@ public class Player {
 	public Vector2Int targetPosition;
 	public GameObject gameObject;
 	public int hp;
+	public List<Card> cards;
 
 	public Player(int id, int hp, Vector2Int position) {
 		this.id = id;
 		this.hp = hp;
-		this.gameObject = gameObject;
 		this.position = position;
 		targetPosition = position;
+
+		Deal();
+	}
+
+	public void UseCard(int actions) {
+		for (int i = 0; i < cards.Count; i++) {
+			if (cards[i].actions == actions) {
+				cards.RemoveAt(i);
+				return;
+			}
+		}
+
+		throw new System.Exception($"Couldn't find card with {actions} actions");
+	}
+
+	void Deal() {
+		cards = new List<Card>() {
+			new Card() { actions = 3 },
+			new Card() { actions = 2 },
+			new Card() { actions = 1 },
+			new Card() { actions = 1 },
+			new Card() { actions = 1 }
+		};
 	}
 
 	public void Move(int horizontal, int vertical) {
--- Assets/Scripts/UI/Views/PlayerView.cs
+++ Assets/Scripts/UI/Views/PlayerView.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using ReactiveUI;
 using TMPro;
+using System.Text;
 
 public class PlayerView : UIView {
 	public TextMeshProUGUI text;
 	public int id;
 
 	public override void Render(UIState state) {
-		text.text = $"Player {id + 1}\nHP: {state.players[id].hp}\nCards: 1 1 1 1 1";
+		if (state.players[id] == null || state.players[id].cards == null) { return; }
+
+		var sb = new StringBuilder();
+		for (int i = 0; i < state.players[id].cards.Count; i++) {
+			sb.Append($"{state.players[id].cards[i].actions} ");
+		}
+		text.text = $"Player {id + 1}\nHP: {state.players[id].hp}\nCards: {sb}";
 	}
 }
