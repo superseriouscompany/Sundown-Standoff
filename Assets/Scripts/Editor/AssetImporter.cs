1b858ece7d4eff699151ddfb448e6b1dc24970dd
Neil Sarkar <neil.r.sarkar@gmail.com>
2019-04-25 09:41:17 -0700

    Add preprocessor script to generate sprites

---
+++ Assets/Scripts/Editor/AssetImporter.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class AssetImporter : AssetPostprocessor {
+	void OnPreprocessTexture() {
+		var textureImporter = (TextureImporter)assetImporter;
+		textureImporter.textureCompression = TextureImporterCompression.Uncompressed;
+		textureImporter.filterMode = FilterMode.Point;
+		textureImporter.spriteImportMode = SpriteImportMode.Multiple;
+		textureImporter.mipmapEnabled = false;
+	}
+
+	// https://forum.unity.com/threads/sprite-editor-automatic-slicing-by-script.320776/
+	void OnPostprocessTexture(Texture2D texture) {
+		Debug.Log("Texture2D: (" + texture.width + "x" + texture.height + ")");
+
+		int spriteSize = 64;
+		int colCount = texture.width / spriteSize;
+		int rowCount = texture.height / spriteSize;
+
+		List<SpriteMetaData> metas = new List<SpriteMetaData>();
+
+		for (int r = 0; r < rowCount; ++r) {
+			for (int c = 0; c < colCount; ++c) {
+				SpriteMetaData meta = new SpriteMetaData();
+				meta.rect = new Rect(c * spriteSize, r * spriteSize, spriteSize, spriteSize);
+				meta.name = c + "-" + r;
+				metas.Add(meta);
+			}
+		}
+
+		Debug.Log($"Got this many sprites: {metas.Count}");
+
+		TextureImporter textureImporter = (TextureImporter)assetImporter;
+		textureImporter.spritesheet = metas.ToArray();
+	}
+
+	void OnPostprocessSprites(Texture2D texture, Sprite[] sprites) {
+		Debug.Log("Sprites: " + sprites.Length);
+	}
+}
