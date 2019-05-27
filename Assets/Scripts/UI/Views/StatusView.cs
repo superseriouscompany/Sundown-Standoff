3038647aa4126114d5d56ae860eb1e7c69baa975
Neil Sarkar <neil.r.sarkar@gmail.com>
2019-05-27 16:29:53 -0700

    Checkpoint: color players and coarse layout for player status boxes

---
+++ Assets/Scripts/UI/Views/StatusView.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ReactiveUI;
+using TMPro;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatusView: UIView {
+	public int id;
+
+	public Image avatarImage;
+	public GameObject heartContainer;
+	public GameObject bulletContainer;
+	public GameObject heartPrefab;
+	public GameObject bulletPrefab;
+
+	public override void Render(UIState state) {
+		Debug.Log($"Hello {id}");
+		avatarImage.color = id == 0 ? Color.blue : Color.green;
+	}
+}
