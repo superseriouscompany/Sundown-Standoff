3970a48ff0a899284670723392ce3269d0efde94
Neil Sarkar <neil.r.sarkar@gmail.com>
2019-05-27 16:40:59 -0700

    Checkpoint: populate hearts and bullets (also hearts) from game engine

--- Assets/Scripts/UI/Views/StatusView.cs
+++ Assets/Scripts/UI/Views/StatusView.cs
@@ -16,7 +16,32 @@ public class StatusView: UIView {
 	public GameObject bulletPrefab;
 
 	public override void Render(UIState state) {
+		var player = state.players[id];
+		if (player == null) { return; }
+
 		Debug.Log($"Hello {id}");
 		avatarImage.color = id == 0 ? Color.blue : Color.green;
+
+		if (heartPrefab != null) {
+			foreach (Transform child in heartContainer.transform) {
+				Destroy(child.gameObject);
+			}
+
+			for (int i = 0; i < player.hp; i++) {
+				var heart = Instantiate(heartPrefab);
+				heart.transform.SetParent(heartContainer.transform);
+			}
+		}
+
+		if (bulletContainer != null) {
+			foreach (Transform child in bulletContainer.transform) {
+				Destroy(child.gameObject);
+			}
+
+			for (int i = 0; i < player.ammo; i++) {
+				var bullet = Instantiate(bulletPrefab);
+				bullet.transform.SetParent(bulletContainer.transform);
+			}
+		}
 	}
 }
