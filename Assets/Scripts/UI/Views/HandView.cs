ba838e5e0a0423ba9d897f9dffae453486a37a69
Neil Sarkar <neil.r.sarkar@gmail.com>
2019-05-27 19:12:12 -0700

    Hook up draw pile cards

--- Assets/Scripts/UI/Views/HandView.cs
+++ Assets/Scripts/UI/Views/HandView.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using ReactiveUI;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HandView : UIView {
-	public TextMeshProUGUI text;
 	public int id;
 
 	public GameObject handContainer;
 	public GameObject cardPrefab;
+
+	public GameObject drawPileContainer;
+	public GameObject minicardPrefab;
 
 	public override void Render(UIState state) {
 		if (state.players[id] == null) { return; }
@@ -22,7 +25,19 @@ public class HandView : UIView {
 		foreach (var card in hand.cards) {
 			var cardBox = Instantiate(cardPrefab);
 			cardBox.transform.SetParent(handContainer.transform, false);
+			cardBox.transform.Find("Card").GetComponent<Image>().sprite = card.sprite;
 			cardBox.transform.Find("Words").transform.Find("Name").GetComponent<TextMeshProUGUI>().text = card.name;
+			cardBox.transform.Find("Words").transform.Find("Description").GetComponent<TextMeshProUGUI>().text = card.description;
+		}
+
+		foreach (Transform child in drawPileContainer.transform) {
+			Destroy(child.gameObject);
+		}
+
+		foreach (var card in hand.library) {
+			var cardBox = Instantiate(minicardPrefab);
+			cardBox.transform.SetParent(drawPileContainer.transform, false);
+			cardBox.GetComponent<Image>().sprite = card.sprite;
 		}
 	}
 }
